2cd7a00e606620ca9975a1eb29bbe0db8db65fe7
qwq713 <qwq713@naver.com>
2023-07-30 00:35:36 +0900

    TROS-37
    - 캐러셀 슬라이드 attribute값 변수화

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -8,6 +8,11 @@ import styles from './page.module.scss';
 
 export default function Home() {
   // const t = useTranslations('Index');
+  const interval: number = 5000;
+  const transitionTime: number = 1500;
+  const imageWidth: number = 16 * 23;
+  const imageHeight: number = 16 * 23;
+
   const images: string[] = [
     '/common/home/img/image_1.jpeg',
     '/common/home/img/image_2.jpeg',
@@ -17,9 +22,20 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <Carousel autoPlay infiniteLoop interval={5000} transitionTime={1500}>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        interval={interval}
+        transitionTime={transitionTime}
+      >
         {images.map((img) => (
-          <Image key={img} src={img} alt="img" width={368} height={368} />
+          <Image
+            key={img}
+            src={img}
+            alt="img"
+            width={imageWidth}
+            height={imageHeight}
+          />
         ))}
       </Carousel>
     </main>
