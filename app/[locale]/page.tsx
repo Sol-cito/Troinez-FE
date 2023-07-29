dadf4b749d1d73bef595677798f563bdddf6aea5
qwq713 <qwq713@naver.com>
2023-07-30 00:31:38 +0900

    TROIS-37 - package.json : react-response-carousel 라이브러리 추가 ( 슬라이더 ) - app/[locale]/page.tsx : 캐러셀 슬라이더 구현 - public/common/home/img/image_1,2.jpeg : 샘플이미지 추가

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,27 @@
 'use client';
 
-import { useTranslations } from 'next-intl';
+// import { useTranslations } from 'next-intl';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image';
+import styles from './page.module.scss';
 
 export default function Home() {
-  const t = useTranslations('Index');
+  // const t = useTranslations('Index');
+  const images: string[] = [
+    '/common/home/img/image_1.jpeg',
+    '/common/home/img/image_2.jpeg',
+    '/common/home/img/image_1.jpeg',
+    '/common/home/img/image_2.jpeg',
+  ];
 
-  return <a href="title">{t('title')}</a>;
+  return (
+    <main className={styles.main}>
+      <Carousel autoPlay infiniteLoop interval={5000} transitionTime={1500}>
+        {images.map((img) => (
+          <Image key={img} src={img} alt="img" width={368} height={368} />
+        ))}
+      </Carousel>
+    </main>
+  );
 }
