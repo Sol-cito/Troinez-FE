f32ad7a227f60cf0bcd5638353c62c9319382bef
qwq713 <qwq713@naver.com>
2023-08-24 09:54:21 +0900

    TROIS-47 naverlogin 관련 환경변수 생성

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -6,9 +6,11 @@ import styles from './page.module.scss';
 
 export default function Login() {
   const naverLoginImage = '/common/login/img/btnG_official.png';
+  const clientId = process.env.NEXT_PUBLIC_NAVER_LOGIN_CLIENT_ID;
+  const redirectUri = process.env.NEXT_PUBLIC_NAVER_LOGIN_REDIRECT_URI;
+
   // eslint-disable-next-line operator-linebreak
-  const naverLoginUri =
-    'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=qwJU3X8aINd3fD0YtXqH&state=hLiDdL2uhPtsftcU&redirect_uri=http://localhost:8080/api/login/oauth2/code/naver';
+  const naverLoginUri = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&state=hLiDdL2uhPtsftcU&redirect_uri=${redirectUri}`;
   return (
     <div className={styles.login_wrapper}>
       <div className={styles.login_left_title}>MEMBERSHIP</div>
