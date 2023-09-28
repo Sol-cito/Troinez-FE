a7690de2145ae1b0913646d8ac61f15f50aece8b
sol <dataenggu@gmail.com>
2023-09-29 01:35:11 +0200

    캐러샐 컴포넌트 변경 props 적용

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,24 @@
 'use client';
 
-import CarouselImages from '@/components/carouselImages/carouselImages';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './page.module.scss';
+import CarouselImages from '@/components/carouselImages/carouselImages';
 
 export default function Home() {
+  const mainImageUrlList: string[] = [
+    '/common/home/img/Main01.jpg',
+    '/common/home/img/Main02.jpg',
+    '/common/home/img/Main03.jpg',
+    '/common/home/img/Main04.jpg',
+  ];
+
   return (
     <div className={styles.home}>
-      <CarouselImages />
+      <CarouselImages
+        autoPlay={true}
+        showIndicators={false}
+        imageUrlList={mainImageUrlList}
+      />
     </div>
   );
 }
