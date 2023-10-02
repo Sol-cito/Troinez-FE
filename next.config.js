0a7e529620435685730fa6d9465086a7bc05581b
sol <dataenggu@gmail.com>
2023-10-02 20:51:33 +0200

    test rewrite

--- next.config.js
+++ next.config.js
@@ -6,13 +6,18 @@ const BASE_API_PORT = process.env.NEXT_PUBLIC_BASE_API_PORT;
 module.exports = {
   async rewrites() {
     console.log('---------rewrite test');
-    console.log(BASE_API_HOST);
-    console.log(BASE_API_PORT);
+    console.log(
+      'https://'
+        .concat(BASE_API_HOST)
+        .concat(':')
+        .concat(BASE_API_PORT)
+        .concat('/api/v1/:path*')
+    );
 
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://'
+        destination: 'https://'
           .concat(BASE_API_HOST)
           .concat(':')
           .concat(BASE_API_PORT)
