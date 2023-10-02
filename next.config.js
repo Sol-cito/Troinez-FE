21a7870b3664622d4b5f5799d70a4de3d45e36c9
sol <dataenggu@gmail.com>
2023-10-02 20:04:46 +0200

    rewrite 테스트

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const BASE_API_HOST = process.env.NEXT_PUBLIC_BASE_API_HOST;
+const BASE_API_PORT = process.env.NEXT_PUBLIC_BASE_API_PORT;
+
 module.exports = {
   async rewrites() {
-    const BASE_API_HOST = process.env.NEXT_PUBLIC_BASE_API_HOST;
-    const BASE_API_PORT = process.env.NEXT_PUBLIC_BASE_API_PORT;
-
     return [
       {
         source: '/api/v1/:path*',
         destination: 'https://'
-          .concat('test')
+          .concat(BASE_API_HOST)
           .concat(':')
-          .concat('1234')
+          .concat(BASE_API_PORT)
           .concat('/api/v1/:path*'),
       },
     ];
