2b82f01b6501674ac4cb713a2cba85d50aedb6c4
dataenggu <dsolkim1030@gmail.com>
2023-11-10 01:03:45 +0100

    About 페이지 구조 변경

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -7,46 +7,78 @@ import styles from './page.module.scss';
 export default function AboutPage() {
   const firstAboutT = useTranslations('About.firstAbout');
   const secondAboutT = useTranslations('About.secondAbout');
+  const thirdAboutT = useTranslations('About.thirdAbout');
+  const fourthAboutT = useTranslations('About.fourthAbout');
+  const fifthAboutT = useTranslations('About.fifthAbout');
 
-  const aboutImage1 = '/common/about/img/About01.png';
-  const aboutImage2 = '/common/about/img/About02.png';
-  const aboutImage3 = '/common/about/img/About03.png';
+  const firstAboutImage = '/common/about/img/01_LOW.jpg';
+  const thirdAboutImage = '/common/about/img/02_LOW.jpg';
+  const fourthAboutImage = '/common/about/img/03_LOW.jpg';
+  const fifthAboutImage = '/common/about/img/04_LOW.jpg';
 
   return (
     <div className={styles.about_wrapper}>
       <div className={styles.flex_box}>
         <div className={styles.image_box}>
           <Image
-            src={aboutImage1}
+            src={firstAboutImage}
             alt="first about image"
             fill
-            sizes="5vw"
             priority
+            sizes="25vw"
           />
         </div>
         <div className={styles.text_box}>
           <p className={styles.title}>{firstAboutT('title')}</p>
-          <p className={styles.sub_title}>{firstAboutT('subTitle1')}</p>
-          <p className={styles.sub_title}>{firstAboutT('subTitle2')}</p>
-          <p>{firstAboutT('retualText')}</p>
-          <p>{firstAboutT('retualTestEng')}</p>
+          <p>{firstAboutT('content')}</p>
+        </div>
+      </div>
+      <div className={styles.text_box}>
+        <p className={styles.title}>{secondAboutT('title')}</p>
+        <p>{secondAboutT('content.sentence1')}</p>
+        <p>{secondAboutT('content.sentence2')}</p>
+        <p>{secondAboutT('content.sentence3')}</p>
+        <p>{secondAboutT('content.sentence4')}</p>
+      </div>
+      <div className={styles.flex_box}>
+        <div className={styles.image_box}>
+          <Image
+            src={thirdAboutImage}
+            alt="second about image"
+            fill
+            sizes="25vw"
+          />
+        </div>
+        <div className={styles.text_box}>
+          <p>{thirdAboutT('content')}</p>
         </div>
       </div>
       <div className={styles.flex_box}>
         <div className={styles.text_box}>
-          <p className={styles.title}>{secondAboutT('title')}</p>
-          <p className={styles.sub_title}>{secondAboutT('subTitle1')}</p>
-          <p className={styles.sub_title}>{secondAboutT('subTitle2')}</p>
-          <p>{secondAboutT('retualText')}</p>
-          <p>{secondAboutT('retualTestEng')}</p>
+          <p className={styles.title}>{fourthAboutT('title')}</p>
+          <p>{fourthAboutT('content')}</p>
         </div>
         <div className={styles.image_box}>
-          <Image src={aboutImage2} alt="second about image" fill />
+          <Image
+            src={fourthAboutImage}
+            alt="third about image"
+            fill
+            sizes="25vw"
+          />
         </div>
       </div>
       <div className={styles.flex_box}>
-        <div className={styles.full_image_box}>
-          <Image src={aboutImage3} alt="third about image" fill />
+        <div className={styles.text_box}>
+          <p className={styles.title}>{fifthAboutT('title')}</p>
+          <p>{fifthAboutT('content')}</p>
+        </div>
+        <div className={styles.image_box}>
+          <Image
+            src={fifthAboutImage}
+            alt="fourth about image"
+            fill
+            sizes="25vw"
+          />
         </div>
       </div>
     </div>
