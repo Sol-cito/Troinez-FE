0d1ba233583c228415493d4765b108465d27e8b2
sol <dataenggu@gmail.com>
2023-11-18 16:25:22 +0300

    반응형 menu dropdown 구현 및 css 적용

--- components/hamburgerMenuBar/hamburgerMenuBar.tsx
+++ components/hamburgerMenuBar/hamburgerMenuBar.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { useState } from 'react';
 import styles from './hamburgerMenuBar.module.scss';
 import Image from 'next/image';
+import { useLocale } from 'next-intl';
 
 export default function HamburgerMenuBar() {
+  const [isHomeClicked, setIsHomeClicked] = useState(false);
+  const [isShopClicked, setIsShopClicked] = useState(false);
+
+  const locale: string = useLocale();
+
+  const onClickHome = () => {
+    setIsHomeClicked(!isHomeClicked);
+  };
+
+  const onClickShop = () => {
+    setIsShopClicked(!isShopClicked);
+  };
+
   return (
     <>
       <div className={styles.menu_bar_wrapper}>
@@ -21,11 +38,58 @@ export default function HamburgerMenuBar() {
             onClick={() => {}}
           />
         </div>
-        <div className={styles.home}>
-          <span className={styles.text}>HOME</span>
+        <div className={styles.home} onClick={onClickHome}>
+          <span className={styles.text}>
+            HOME({locale.toLocaleUpperCase()})
+          </span>
+          <Image
+            className={styles.arrow_image}
+            src={
+              isHomeClicked
+                ? '/common/icon/arrow-up.svg'
+                : '/common/icon/arrow-down.svg'
+            }
+            alt="TNZ"
+            width={10}
+            height={10}
+          />
         </div>
-        <div className={styles.shop}>
+        {/* {isHomeClicked && ( */}
+        <div
+          className={styles.lang_selection_dropdown}
+          style={{
+            visibility: isHomeClicked ? 'visible' : 'hidden',
+            height: isHomeClicked ? 'max-content' : 0,
+          }}
+        >
+          <span>Korean</span>
+          <span>English</span>
+        </div>
+        {/* )} */}
+        <div className={styles.shop} onClick={onClickShop}>
           <span className={styles.text}>SHOP</span>
+          <Image
+            className={styles.arrow_image}
+            src={
+              isShopClicked
+                ? '/common/icon/arrow-up.svg'
+                : '/common/icon/arrow-down.svg'
+            }
+            alt="TNZ"
+            width={10}
+            height={10}
+          />
+        </div>
+        <div
+          className={styles.shop_selection_dropdown}
+          style={{
+            visibility: isShopClicked ? 'visible' : 'hidden',
+            height: isShopClicked ? 'max-content' : 0,
+          }}
+        >
+          <span>All Products</span>
+          <span>Room Diffuser</span>
+          <span>Car Diffuser</span>
         </div>
         <div className={styles.about}>
           <span className={styles.text}>ABOUT</span>
